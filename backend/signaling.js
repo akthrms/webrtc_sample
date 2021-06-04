8d83a25405eb86a4741bf86f1c6f26d6af44f47b
akthrms <akt.hrms@gmail.com>
2021-06-05 06:03:49 +0900

    update

--- backend/signaling.js
+++ backend/signaling.js
@@ -1,16 +1,37 @@
 "use strict";
 
-const WebSocketServer = require("ws").Server;
+function log(message) {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
 
+  console.log(
+    `[${year}/${month}/${day} ${hour}:${minute}:${second}] ${message}`
+  );
+}
+
+// WebSocket サーバーを起動する
+const WebSocketServer = require("ws").Server;
 const port = 3001;
 const webSocketServer = new WebSocketServer({ port: port });
 
 webSocketServer.on("connection", (webSocket) => {
+  log("WebSocket server is connected.");
+
   webSocket.on("message", (message) => {
+    log(`message is received.`);
+
     webSocketServer.clients.forEach((client) => {
       if (webSocket !== client) {
+        // そのまま転送する
         client.send(message);
       }
     });
   });
 });
+
+log(`WebSocket server is started. port = ${port}`);
